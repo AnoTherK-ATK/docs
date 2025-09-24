da37706c801eaa06661106c2b8a17b5d324d7511
AnoTherK-ATK <khanhvu212@gmail.com>
2025-09-24 12:17:59 +0700

    about me and some changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,19 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: "AnoTherK's shitpost",
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/AnoTherK-ATK' }, { icon: 'facebook', label: 'Facebook', href: 'https://www.facebook.com/anotherk.da' }],
 			sidebar: [
 				{
-					label: 'Guides',
+					label: 'About',
 					items: [
 						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
+						{ label: 'Tôi là ai', slug: 'about/me' },
 					],
 				},
 				{
@@ -21,8 +22,54 @@ export default defineConfig({
 					autogenerate: { directory: 'reference' },
 				},
 			],
+			head: [
+				{
+					tag: 'script',
+					attrs: {
+						id: 'mathjax-script-config',
+						type: 'text/javascript',
+					},
+					// Defines how MathJax finds and processes math in your content
+					content: `
+						window.MathJax = {
+						tex: {
+							inlineMath: [['$', '$']],
+							displayMath: [['$$', '$$']],
+							packages: {'[+]': ['ams']}
+						},
+						svg: {
+							fontCache: 'global'
+						}
+						};
+					`,
+				},
+				{
+					tag: 'script',
+					attrs: {
+						id: 'mathjax-script',
+						type: 'text/javascript',
+						src: 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js',
+						async: true,
+					},
+				},
+				{
+					tag: 'script',
+					attrs: {
+						type: 'module',
+					},
+					// Initializes MermaidJS and sets a theme that works well with Starlight
+					content: `
+						import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@10/dist/mermaid.esm.min.mjs';
+						mermaid.initialize({
+						startOnLoad: true,
+						theme: 'dark', // Or 'default', 'neutral', 'forest'
+						});
+					`,
+				},
+			],
+			customCss: ['./src/mathjax.css'],
 		}),
 	],
 	site: 'https://AnoTherK-ATK.github.io',
-	base: '/repo'
+	base: '/docs'
 });
---
+++ src/content/docs/about/about.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Tôi chả biết nên ghi gì trong này\n";
+    return 0;
+}
