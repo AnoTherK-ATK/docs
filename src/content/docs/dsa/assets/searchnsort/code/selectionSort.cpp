c54b3df2632db9a1d719ca9e28b2bd8b540208f3
AnoTherK-ATK <khanhvu212@gmail.com>
2025-10-27 17:29:26 +0700

    added selection sort

--- src/content/docs/dsa/assets/searchnsort/code/selectionSort.cpp
+++ src/content/docs/dsa/assets/searchnsort/code/selectionSort.cpp
@@ -1,6 +1,10 @@
 void selectionSort(int a[], int n){
     for(int i = 0; i < n; i++){
-        int indexMin = std::min_element(a + i, a + n) - a;
-        std::swap(a[i], a[indexMin]);
+        int indexMin = 0;
+        for(int j = i; j < n; j++){
+            if(a[j] < a[indexMin])
+                indexMin = j;
+        }
+        std::swap(a[i], a[j]);
     }
 }
